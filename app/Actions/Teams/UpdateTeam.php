b5c78e6e4195ebcce767bcba8d60171f9696b0be
Huzaifa-Adil <huzaifa.adil@devparagon.com>
2025-06-18 15:33:12 +0500

    update team activity done

--- app/Actions/Teams/UpdateTeam.php
+++ app/Actions/Teams/UpdateTeam.php
@@ -3,11 +3,19 @@
 namespace App\Actions\Teams;
 
 use App\Models\Team;
+use App\Models\User;
 
 class UpdateTeam
 {
-    public static function handle(array $data, Team $team): void
+    public static function handle(array $data, Team $team, User $user): void
     {
         $team->update($data);
+
+        activity()
+            ->causedBy($user)
+            ->performedOn($team)
+            ->event('Team Updated')
+            ->withProperties(['attributes' => $team->toArray()])
+            ->log(':causer.name updated the team.');
     }
 }
