a3b8b04bb8b1aa145c9aa4382978b15573fd84e0
Huzaifa-Adil <huzaifa.adil@devparagon.com>
2025-06-18 15:55:12 +0500

    team invitation deleted activity done

--- app/Actions/Teams/TeamInvitation/DeleteTeamInvitation.php
+++ app/Actions/Teams/TeamInvitation/DeleteTeamInvitation.php
@@ -3,11 +3,19 @@
 namespace App\Actions\Teams\TeamInvitation;
 
 use App\Models\TeamInvitation;
+use App\Models\User;
 
 class DeleteTeamInvitation
 {
-    public static function handle(TeamInvitation $teamInvitation): void
+    public static function handle(TeamInvitation $teamInvitation, User $user): void
     {
         $teamInvitation->delete();
+
+        activity()
+            ->causedBy($user)
+            ->performedOn($teamInvitation)
+            ->event('Team Invitation Deleted')
+            ->withProperties(['attributes' => $teamInvitation->toArray()])
+            ->log(':causer.name deleted the team invitation');
     }
 }
