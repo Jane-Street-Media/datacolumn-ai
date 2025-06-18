7962a7246c4aacb7b733030ab38b6ca3e638d1a1
Huzaifa-Adil <huzaifa.adil@devparagon.com>
2025-06-18 11:43:00 +0500

    concurrent added on stats

--- app/Actions/Dashboard/GetStats.php
+++ app/Actions/Dashboard/GetStats.php
@@ -5,18 +5,19 @@ namespace App\Actions\Dashboard;
 use App\Actions\GetRecentProjects;
 use App\Enums\Period;
 use App\Helpers\ValueMetric;
+use Illuminate\Support\Facades\Concurrency;
 
 class GetStats
 {
     public static function handle(): array
     {
-        $projectStats = ValueMetric::query(GetRecentProjects::execute())->for(Period::LAST_30_DAYS, true)->count();
+        [$projectStats, $teamMemberStats, $chartStats] = Concurrency::run([
+            fn () => ValueMetric::query(GetRecentProjects::execute())->for(Period::LAST_30_DAYS, true)->count(),
+            fn () => ValueMetric::query(GetTeamMembers::handle())->for(Period::LAST_30_DAYS, true)->count(),
+            fn () => ValueMetric::query(GetCharts::handle())->for(Period::LAST_30_DAYS, true)->count(),
+        ]);
         $projectStats['percentage_change'] = ValueMetric::calculatePercentageChange($projectStats['value'], $projectStats['compare_value']);
-
-        $teamMemberStats = ValueMetric::query(GetTeamMembers::handle())->for(Period::LAST_30_DAYS, true)->count();
         $teamMemberStats['percentage_change'] = ValueMetric::calculatePercentageChange($teamMemberStats['value'], $teamMemberStats['compare_value']);
-
-        $chartStats = ValueMetric::query(GetCharts::handle())->for(Period::LAST_30_DAYS, true)->count();
         $chartStats['percentage_change'] = ValueMetric::calculatePercentageChange($chartStats['value'], $chartStats['compare_value']);
 
         return [
