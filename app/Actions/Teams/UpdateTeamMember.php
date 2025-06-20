b271ea73adea7170bd2f0599eb7e66292c548b7b
AsharIrfan <asharirfan26@gmail.com>
2025-06-20 18:06:42 +0500

    added return types

--- app/Actions/Teams/UpdateTeamMember.php
+++ app/Actions/Teams/UpdateTeamMember.php
@@ -7,13 +7,14 @@ use App\Models\User;
 
 class UpdateTeamMember
 {
-    public static function handle(array $data, User $user): void
+    public static function handle(array $data, User $user): User
     {
         $user->syncRoles($data['role']);
-        defer(fn () => activity()
+        defer(fn() => activity()
             ->performedOn($user)
             ->event(ActivityEvents::TEAM_MEMBER_UPDATED->value)
             ->log(":causer.name updated a team member named {$user->name}.")
         );
+        return $user;
     }
 }
