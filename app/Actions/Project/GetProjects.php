3ff09cea875a7736658ecc9cacf4feb0321a01ca
AsharIrfan <asharirfan26@gmail.com>
2025-06-20 15:33:37 +0500

    added return type

--- app/Actions/Project/GetProjects.php
+++ app/Actions/Project/GetProjects.php
@@ -3,10 +3,11 @@
 namespace App\Actions\Project;
 
 use App\Models\Project;
+use Illuminate\Support\Collection;
 
 class GetProjects
 {
-    public static function handle(array $data)
+    public static function handle(array $data):Collection
     {
         return Project::withCount('charts')
             ->when(isset($data['folder']), fn ($q) => $q->whereRelation('folder', 'id', $data['folder']))
