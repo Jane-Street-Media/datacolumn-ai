c1882a4429d9aa62bc27456649bc818899c2483f
Huzaifa-Adil <huzaifa.adil@devparagon.com>
2025-06-23 09:31:35 +0500

    implemented ShouldQueue in invitation sent mail

--- resources/views/mail/invitation-sent.blade.php
+++ resources/views/mail/invitation-sent.blade.php
@@ -1,18 +1,16 @@
 <x-mail::message>
-    {{ __('You have been invited to join the :team team!', ['team' => $invitation->team->name]) }}
+    # {{ __('You have been invited to join the :team team!', ['team' => $invitation->team->name]) }}
 
-    {{ __('If you do not have an account, you may create one by clicking the button below. After creating an account, you may click the invitation acceptance button in this email to accept the team invitation:') }}
+    {{ __('If you do not have an account, you may create one by clicking the button below. After creating an account, you may click the invitation acceptance button in this email to accept the team invitation.') }}
 
     <x-mail::button :url="route('register')">
         {{ __('Register Account') }}
     </x-mail::button>
 
-
-
     <x-mail::button :url="$acceptUrl">
         {{ __('Accept Invite') }}
     </x-mail::button>
 
-    Thanks,<br>
+    {{ __('Thanks') }},
     {{ config('app.name') }}
 </x-mail::message>
