1346d38709897082261a51d7dc52ca344a6d79da
Huzaifa-Adil <huzaifa.adil@devparagon.com>
2025-06-26 10:38:37 +0500

    icon code clean

--- resources/js/pages/settings/subscription.tsx
+++ resources/js/pages/settings/subscription.tsx
@@ -4,7 +4,7 @@ import ChargebeeBanner from '@/pages/banners/chargebeeBanner';
 import Pricing from '@/pages/pricing/pricing';
 import { BreadcrumbItem, SharedData } from '@/types';
 import { Head, router, usePage } from '@inertiajs/react';
-import { Check, CreditCard, Loader2 } from 'lucide-react';
+import { Check, CreditCard, Loader2, XCircle } from 'lucide-react';
 import React, { useMemo, useState } from 'react';
 import { toast } from 'sonner';
 
@@ -277,18 +277,7 @@ const SubscriptionSettings: React.FC = ({ subscription: subscription, plans }) =
                                                 </span>
                                             ) : (
                                                 <span className="flex items-center justify-center">
-                                                    <svg
-                                                        className="mr-2 h-5 w-5"
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        viewBox="0 0 20 20"
-                                                        fill="currentColor"
-                                                    >
-                                                        <path
-                                                            fillRule="evenodd"
-                                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                                            clipRule="evenodd"
-                                                        />
-                                                    </svg>
+                                                    <XCircle className="mr-2 h-5 w-5"/>
                                                     Resume Subscription
                                                 </span>
                                             )}
