7e477f222a6490372073ec4126d6ec1208742329
Huzaifa-Adil <huzaifa.adil@devparagon.com>
2025-07-01 19:17:46 +0500

    extra code dle

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -11,14 +11,6 @@ class Plan extends Model
 
     protected $table = 'plans';
 
-    const FREE_PLAN = 'free plan';
-
-    public const FREE_PLAN_NO_OF_FOLDERS = 3;
-
-    public const FREE_PLAN_NO_OF_PROJECTS = 3;
-
-    public const FREE_PLAN_NO_OF_INVITATIONS = 3;
-
     protected $fillable = [
         'chargebee_id',
         'display_name',
