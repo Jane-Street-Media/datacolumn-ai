9c534a7a451a765905f4b56266fe510ed019b5b5
Huzaifa-Adil <huzaifa.adil@devparagon.com>
2025-07-02 19:09:05 +0500

    CODE CLEAN

--- app/Actions/SyncSubscriptionPlanChanges.php
+++ app/Actions/SyncSubscriptionPlanChanges.php
@@ -14,39 +14,44 @@ class SyncSubscriptionPlanChanges
 {
     public static function handle(User $user): void
     {
-
         $currentPlanFeatures = $user->currentTeam->subscriptionWithProductDetails()->plan->features;
         $projectLimit = $currentPlanFeatures[PlanFeatureEnum::NO_OF_PROJECTS->value];
         $chartLimit = $currentPlanFeatures[PlanFeatureEnum::NO_OF_CHARTS->value];
         $teamMembersLimit = $currentPlanFeatures[PlanFeatureEnum::NO_OF_TEAM_MEMBERS->value];
 
-        $totalProjectsCount = GetProjects::handle()->count();
-        if ($projectLimit !== -1 && $totalProjectsCount > $projectLimit) {
-            $excessProjectCount = $totalProjectsCount - $projectLimit;
-            defer(fn() => GetProjects::handle()
-                ->latest()
-                ->limit($excessProjectCount)
-                ->update([
-                    'status' => ProjectStatus::INACTIVE,
-                ]));
+        if ($projectLimit !== -1) {
+            $totalProjectsCount = GetProjects::handle()->count();
+            if ($totalProjectsCount > $projectLimit) {
+                $excessProjectCount = $totalProjectsCount - $projectLimit;
+                defer(fn() => GetProjects::handle()
+                    ->latest()
+                    ->limit($excessProjectCount)
+                    ->update([
+                        'status' => ProjectStatus::INACTIVE,
+                    ]));
+            }
         }
 
-        $totalChartsCount = GetChartQuery::handle()->count();
-        if ($chartLimit !== -1 && $totalChartsCount > $chartLimit) {
-            $excessChartCount = $totalChartsCount - $chartLimit;
-            defer(fn() => GetChartQuery::handle()
-                ->latest()
-                ->limit($excessChartCount)
-                ->update([
-                    'status' => ChartStatus::INACTIVE,
-                ]));
+
+        if ($chartLimit !== -1) {
+            $totalChartsCount = GetChartQuery::handle()->count();
+            if ($totalChartsCount > $chartLimit) {
+                $excessChartCount = $totalChartsCount - $chartLimit;
+                defer(fn() => GetChartQuery::handle()
+                    ->latest()
+                    ->limit($excessChartCount)
+                    ->update([
+                        'status' => ChartStatus::INACTIVE,
+                    ]));
+            }
         }
 
-        $totalTeamMembersCount = $user->currentTeam->invitations()->count() + $user->currentTeam->users()->count();
-        if ($teamMembersLimit !== -1 && $totalTeamMembersCount > $teamMembersLimit) {
-            $excessTeamMembersCount = $totalTeamMembersCount - $teamMembersLimit;
-            defer(fn() => TeamInvitation::query()->latest()->limit($excessTeamMembersCount)->delete());
+        if ($teamMembersLimit !== -1) {
+            $totalTeamMembersCount = $user->currentTeam->invitations()->count() + $user->currentTeam->users()->count();
+            if ($totalTeamMembersCount > $teamMembersLimit) {
+                $excessTeamMembersCount = $totalTeamMembersCount - $teamMembersLimit;
+                defer(fn() => TeamInvitation::query()->latest()->limit($excessTeamMembersCount)->delete());
+            }
         }
-
     }
 }
