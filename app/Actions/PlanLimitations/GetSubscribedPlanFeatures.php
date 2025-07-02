56eb71bc64a5052e79e67c5e68b5cd15e23439b1
Huzaifa-Adil <huzaifa.adil@devparagon.com>
2025-07-02 19:00:27 +0500

    code refactor

--- app/Actions/PlanLimitations/GetSubscribedPlanFeatures.php
+++ app/Actions/PlanLimitations/GetSubscribedPlanFeatures.php
@@ -10,12 +10,6 @@ class GetSubscribedPlanFeatures
 {
     public static function handle(Team $team)
     {
-        $subscription = $team->subscriptionWithProductDetails();
-
-        if (! $subscription) {
-            return Plan::where('chargebee_id', 'free-monthly')->first()->features;
-        }
-
-        return Plan::where('chargebee_id', $subscription->chargebee_price)->first()->features;
+        return $team->subscriptionWithProductDetails()->plan->features;
     }
 }
