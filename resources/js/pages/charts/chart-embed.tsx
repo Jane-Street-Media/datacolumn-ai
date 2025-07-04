38cc290229bd8a8f7a43267d249e78195602c53c
Aamish26622 <aamishirfan2662@gmail.com>
2025-07-04 16:38:21 +0500

    embed chart fix

--- resources/js/pages/charts/chart-embed.tsx
+++ resources/js/pages/charts/chart-embed.tsx
@@ -1,38 +1,27 @@
 import { ChartRenderer } from '@/components/chart-editor/chart-renderer';
+import { ChartEditorProvider } from '@/contexts/chart-editor-context';
 
-export default function ChartEmbed({ config, data }){
+export default function ChartEmbed({ chart }){
 
     const showWatermark = true;
 
     const theme = 'dark';
 
-    const chart = {
-        data: data,
-        config: {
-            ...config,
-            theme: theme as 'light' | 'dark' | 'system'
-        }
-    };
-
-    console.log('chart');
-    console.log(chart);
-
     return (
-        <div className="min-h-screen bg-white p-4">
-            {chart && (
-                <div className="h-full">
-                    <ChartRenderer
-                        data={chart.data}
-                        config={chart.config}
-                    />
+        <ChartEditorProvider chart={chart}>
+            <div className="min-h-screen bg-white p-4">
+                {chart && (
+                    <div className="h-full">
+                        <ChartRenderer />
 
-                    {showWatermark && (
-                        <div className="mt-2 text-right text-xs text-foreground">
-                            Powered by <a href="https://datacolumn.ai" target="_blank" rel="noopener noreferrer" className="text-primary">DataColumn.ai</a>
-                        </div>
-                    )}
-                </div>
-            )}
-        </div>
+                        {showWatermark && (
+                            <div className="mt-2 text-right text-xs text-foreground">
+                                Powered by <a href="https://datacolumn.ai" target="_blank" rel="noopener noreferrer" className="text-primary">DataColumn.ai</a>
+                            </div>
+                        )}
+                    </div>
+                )}
+            </div>
+        </ChartEditorProvider>
     );
 };
