8d9a2a8076c057eea737c67655e80fe453dc9f05
Aamish26622 <aamishirfan2662@gmail.com>
2025-07-04 16:44:28 +0500

    chart inactive not found

--- app/Http/Controllers/EmbeddedChartController.php
+++ app/Http/Controllers/EmbeddedChartController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\ChartStatus;
+use App\Enums\ProjectStatus;
 use App\Models\Chart;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class EmbeddedChartController extends Controller
 {
-    public function __invoke(Chart $chart)
+    public function __invoke(Chart $chart): Response
     {
+        if ($chart->status !== ChartStatus::ACTIVE || $chart->project->status !== ProjectStatus::PUBLISHED) {
+            abort(404, 'Chart not found or is not active.');
+        }
         $chart->increment('total_visits');
         return Inertia::render('charts/chart-embed', [
             'chart' => $chart,
