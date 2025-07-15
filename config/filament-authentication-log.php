9249e3e34519b3816356b67ad1ec919f8cf849ac
Aamish26622 <aamishirfan2662@gmail.com>
2025-07-15 15:30:29 +0500

    commit

---
+++ config/filament-authentication-log.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    // 'user-resource' => \App\Filament\Resources\UserResource::class,
+    'resources' => [
+        'AutenticationLogResource' => \Tapp\FilamentAuthenticationLog\Resources\AuthenticationLogResource::class,
+    ],
+
+    'authenticable-resources' => [
+        \App\Models\User::class,
+    ],
+
+    'authenticatable' => [
+        'field-to-display' => 'name',
+    ],
+
+    'navigation' => [
+        'authentication-log' => [
+            'register' => true,
+            'sort' => 1,
+            'icon' => 'heroicon-o-shield-check',
+            // 'group' => 'Logins',
+        ],
+    ],
+
+    'sort' => [
+        'column' => 'login_at',
+        'direction' => 'desc',
+    ],
+];
