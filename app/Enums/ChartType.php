c2c9ce63c1f6d62b5ff07738fd6fe2c9cbe94719
Curtis <31999619+jsm350@users.noreply.github.com>
2025-07-19 19:03:29 -0400

    Update ChartType.php

--- app/Enums/ChartType.php
+++ app/Enums/ChartType.php
@@ -7,7 +7,18 @@ use App\Traits\EnumToArray;
 enum ChartType: string
 {
     use EnumToArray;
+    
     case AREA = 'area';
     case LINE = 'line';
     case BAR = 'bar';
+    case PIE = 'pie';
+    case SCATTER = 'scatter';
+    case RADAR = 'radar';
+    case RADIAL_BAR = 'radialBar';
+    case FUNNEL = 'funnel';
+    case TREEMAP = 'treemap';
+    case COMPOSED = 'composed';
+    case STACKED_BAR = 'stackedBar';
+    case STACKED_AREA = 'stackedArea';
+    case WATERFALL = 'waterfall';
 }
